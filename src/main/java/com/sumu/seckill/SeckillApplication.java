e37907fac4adb3a987feb7c1556da84d4b9a6225
SuMu <SuMu31707@163.com>
2021-05-06 10:27:18 +0800

    '测试页面跳转'

--- src/main/java/com/sumu/seckill/SeckillApplication.java
+++ src/main/java/com/sumu/seckill/SeckillApplication.java
@@ -1,9 +1,11 @@
 package com.sumu.seckill;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.sumu.seckill.pojo")
 public class SeckillApplication {
 
     public static void main(String[] args) {
