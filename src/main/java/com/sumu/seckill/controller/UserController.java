af74831a7e776ef6e9c8ecfcac6a13198a9d6961
SuMu <SuMu31707@163.com>
2021-05-07 12:00:47 +0800

    添加携带用户信息对接口测试

--- src/main/java/com/sumu/seckill/controller/UserController.java
+++ src/main/java/com/sumu/seckill/controller/UserController.java
@@ -1,12 +1,15 @@
 package com.sumu.seckill.controller;
 
 
+import com.sumu.seckill.pojo.User;
+import com.sumu.seckill.vo.RespBean;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author SuMu
@@ -15,5 +18,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    /**
+     * 用户信息（测试）
+     * 优化前：
+     *      Win：1152.1 QBS
+     *      Linux：906.3 QBS
+     * 优化后：
+     *      Win：
+     *      Linux：
+     * @param user
+     * @return
+     */
+    @RequestMapping("/info")
+    @ResponseBody
+    public RespBean info(User user) {
+        return RespBean.success(user);
+    }
 
 }
--- src/main/java/com/sumu/seckill/vo/RespBeanEnum.java
+++ src/main/java/com/sumu/seckill/vo/RespBeanEnum.java
@@ -21,7 +21,7 @@ public enum RespBeanEnum {
     BIND_ERROR(500212, "参数校验异常"),
     // 秒杀模块5005xx
     EMPTY_STOCK(500500, "库存不足"),
-    REPEATE_ERROR(500501, "该商品没人限购一件");
+    REPEATE_ERROR(500501, "该商品每人限购一件");
 
     private final Integer code;
     private final String message;
